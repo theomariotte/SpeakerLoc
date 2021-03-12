WIP : cangde TDOA calculation + correct some bugs

src/doa/doa_MLE.py
@@ -23,33 +23,51 @@ for i in range(8):
 
 # Synthetic data
 wav_dir = "./data/Synth_data/output/"
-audio_names = ["IS1000a_TR300_T10_nch4_snrinf_ola1_noise0"]
+audio_names = ["IS1000a_TR300_T31_nch4_snr15_ola1_noise0"]
 
 # True DOAs
 #doa_ref = [45.,135.,225.,315.]
 doa_ref = [45.,125.]
 nb_mic = 4
 typ = "ULA"
+# in case of ULA
 x_start = -0.1
 x_stop = 0.1
+
+#in case of Circular array
+theta_start = 0
+theta_stop = 360
+radius = 0.1
 
 sig = WaveProcessorSlidingWindow(wav_dir=wav_dir,
                                  audio_names=audio_names)
 
 
 # wavform framing parameters
-winlen = 16000
+winlen = 256
 winshift = winlen//2
 
 # number of snapshots for doa estimation (i.e. number of frame)
 duration = 20.
 num_snapshot = int(0.5*duration*16000//winlen)
-#num_snapshot = 50
-
+num_snapshot = 10
 
 sig.load(winlen=winlen, shift=winshift)
 fs = sig.getFs()
 num_frame = sig.frameNumber()
+
+# frequency vector
+freq = np.linspace(0.0,fs/2,int(winlen/2))
+
+# Frequency band to be studied
+fmin = 100
+fmax = fs/2
+tmp_inf = np.abs(freq-fmin)
+tmp_sup = np.abs(freq-fmax)
+f_start_idx = np.where(tmp_inf == min(tmp_inf))[0]
+f_stop_idx = np.where(tmp_sup == min(tmp_sup))[0]
+freq_idx_vec = np.arange(f_start_idx, f_stop_idx+1).astype(np.int32)
+nb_freq = len(freq_idx_vec)
 
 
 if typ == "circular":
@@ -91,9 +109,6 @@ start = 0
 step = 5
 stop = 180
 r = 1
-nfft = winlen//2+1
-f_start_idx = 20
-ff = np.linspace(f_start_idx, winlen//2, nfft).astype(np.int32)
 
 grid = CircularGrid2D(theta_start=start,
                       theta_stop=stop,
@@ -101,14 +116,16 @@ grid = CircularGrid2D(theta_start=start,
                       radius=r)
 
 
-rtf = grid.getRTF(freq=ff,
+rtf = grid.getRTF(freq=freq,
                   fs=fs,
-                  array=mic_array)
+                  array=mic_array,
+                  freq_idx_vec=freq_idx_vec)
 coord = grid.components()
 theta = coord["theta"]
 theta *= 180. / np.pi
 num_src = grid.shape()[0]
 
+"""
 # baseline beamforming
 doaBaseline = DoaDelayAndSumBeamforming(microphone_array=mic_array,
                                         grid=grid,
@@ -125,18 +142,10 @@ for idx in range(sig.numel()):
     plt.polar(theta_plt,doaMap)
     #plt.xlabel("DOA [°]")
     #plt.ylabel("power")
-    plt.show(block=False)
-    plt.pause(1.0)
-    plt.close(fig)
-
-# frequency band used for loaclization
-idx_fmin = 50
-idx_fmax = len(ff)-1
-freq_idx_vec = np.linspace(idx_fmin, idx_fmax, nfft-idx_fmin).astype(np.int32)
-nb_freq = len(freq_idx_vec)
-
-
-
+    #plt.show(block=False)
+    #plt.pause(1.0)
+    #plt.close(fig)
+"""
 
 # threshold for confidence measure
 thres = 1.0
@@ -153,7 +162,7 @@ for snp_idx in range(nb_loop):
     idx_frame_stop = idx_frame_start + num_snapshot
     H = doaEngine.broadBandHistogram(idx_frame_start=idx_frame_start,
                                      idx_frame_stop=idx_frame_stop,
-                                     freq=ff,
+                                     freq=freq,
                                      snr_thres=thres,
                                      freq_idx_vec=freq_idx_vec)
 
@@ -164,7 +173,8 @@ for snp_idx in range(nb_loop):
     
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    plt.plot(theta, H,ref_src1,[0,50],"k--")#,ref_src2,[0,50],"k--",ref_src3,[0,50],"k--",ref_src4,[0,50],"k--")
+    plt.plot(theta, H/np.max(np.abs(H)))
+    plt.plot([doa_ref,doa_ref],[0,1],"k--")
     plt.xlabel("DOA [°]")
     plt.ylabel("Likelihood")
     plt.grid()

src/doa/doa_estimator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import logging
+import matplotlib.pyplot as plt
 
 from typing import Optional
 from wave_reader import WaveProcessorSlidingWindow
@@ -44,7 +45,7 @@ class DoaMLE(DoaBase):
                            freq_idx_vec=None):
 
         if freq_idx_vec is None:
-            freq_idx_vec = range(0, len(freq)-1, len(freq))
+            freq_idx_vec = range(len(freq))
 
         nb_freq = len(freq_idx_vec)
         num_src = self.grid.shape()[0]
@@ -62,11 +63,16 @@ class DoaMLE(DoaBase):
             # Broad band histogram
             H = np.zeros((num_src,))
             k = 0
+            sigma_sig = []
+            sigma_nn = []
             for f_idx in freq_idx_vec:
-                log_spectrum, snr = self.singleNarrowBandSpectrum(frame=xx,
+                log_spectrum, snr, ss2,sv2 = self.singleNarrowBandSpectrum(frame=xx,
                                                                   rtf=rtf,
                                                                   nb_freq=nb_freq,
                                                                   freq_index=f_idx)
+
+                sigma_sig.append(ss2)
+                sigma_nn.append(sv2)
 
                 #TODO (21/03/06) temporary remove first value -> problem here !
                 log_spectrum = log_spectrum[1:]
@@ -88,7 +94,13 @@ class DoaMLE(DoaBase):
                 # update histogram
                 H += q_whole * X
                 k += 1
-
+            """
+            plt.figure()
+            plt.plot(freq[freq_idx_vec],sigma_sig)#,freq[freq_idx_vec],sigma_nn,'r')
+            #plt.legend("Signal DSP", "Noise DSP")
+            plt.grid()
+            plt.show()
+            """
         return H
 
     def singleNarrowBandSpectrum(self,
@@ -114,15 +126,20 @@ class DoaMLE(DoaBase):
         for ii in range(src_num):
 
             # RTF at the current frequency bin
-            d = rtf[ii, freq_index, :]
+            d = rtf[ii, freq_index,:]
             d = d[:, np.newaxis]
 
             #pseudo inverse of d
-            d_inv = np.linalg.pinv(d)
-            db_inv = np.dot(d_inv,B_inv)
+            #db = np.dot(B,d)
+            #db_inv = np.linalg.pinv(db)
+            tmp_1 = np.dot(d.conj().T,B_inv)
+            tmp_2 = 1.0/( np.dot( d.conj().T , np.dot( B_inv, d )) )
+            db_inv = tmp_2 * tmp_1
+            #err_ = np.mean(np.mean(np.abs(db_inv-db_inv_2)**2))
+            #db_inv = np.dot(d_inv,B_inv)
 
             # projection of d
-            P = np.dot(d, db_inv)
+            P = np.dot(d, db_inv)   
 
             # orthogonal projection of d
             P_orth = np.eye(mic_num) - P
@@ -136,14 +153,14 @@ class DoaMLE(DoaBase):
             prod_ = np.dot(db_inv, np.dot(tmp_, db_inv.conj().T))
             sigma_s2 = prod_[0][0].astype(float)
 
-            spectrum = np.dot(d, sigma_s2*d.conj().T) + sigma_v2 * B
+            spectrum = np.dot(d, sigma_s2*d.conj().T) + sigma_v2* B
             log_spectrum[ii] = np.log(np.linalg.det(spectrum))
 
             # a posteriori SNR for confidence measure when weighting broadband histogram
             Sigma_V = sigma_v2 * B
             aSNR = np.dot(z.conj().T, z)/np.trace(Sigma_V).astype(float)
 
-        return 1./log_spectrum, aSNR[0][0]
+        return 1./log_spectrum, aSNR[0][0], sigma_s2, sigma_v2
 
 class DoaDelayAndSumBeamforming(DoaBase):
 

src/doa/grid.py
@@ -66,19 +66,31 @@ class CircularGrid2D:
             raise Exception(
                 "<array> parameter should be a microphone array object !")
 
-        # distance between reference mic and others in the array
-        dist = mic_array.getDistance(ref_mic_idx)
+        # microphone coordinates
+        x_mic, y_mic, _ = mic_array.getCoordinates()
+
+        # sources coordinates
+        x_src = self.radius * np.cos(self.theta)
+        y_src = self.radius * np.sin(self.theta)
+
+        #distance between each source and each microphone
+        R = np.sqrt( (x_mic[:,np.newaxis].T - x_src[:,np.newaxis])**2 + (y_mic[:,np.newaxis].T - y_src[:,np.newaxis])**2 )
+        tdoa = R/c0
+
+        ## distance between reference mic and others in the array
+        #dist = mic_array.getDistance(ref_mic_idx)
     
         # compute TDOA for each microphone
-        tdoa = dist[:, np.newaxis] * np.cos(self.theta[:,np.newaxis].T) / c0 
+        #doa = dist[:, np.newaxis] * np.cos(self.theta[:,np.newaxis].T) / c0 
         #tdoa = np.matmul(dist[:, np.newaxis], self.theta[:, np.newaxis].T).T
 
-        return tdoa.T
+        return tdoa
 
     def getRTF(self,
                freq,
                fs,
                array,
+               freq_idx_vec: Optional[None]=None,
                reference_idx: Optional[int] = 0,
                c0: Optional[float] = 343.0):
         """
@@ -87,9 +99,13 @@ class CircularGrid2D:
         :param freq: np array containing frequencies where RTF is evaluated
         :param fs: sampling rate
         :param array: microphone array (as MicArray object) 
+        :param freq_idx_vec: array containing indexes of frequency to be used (default None - all frequencies in <freq> are considered)
         :param reference_idx: reference microphone index for relative delay calculation (default : 0)
         :param c0: speed of sound in m/s (default: 343.0)
         """
+        # working frequency band
+        if freq_idx_vec is not None:
+            freq = freq[freq_idx_vec]
 
         aliasing = np.where(freq > fs/2)[0]
         if aliasing.shape[0] > 0:
@@ -135,7 +151,7 @@ if __name__ == "__main__":
 
     start = 0
     step = 5
-    stop = 360
+    stop = 180
     r = 0.6
     fs = 16e3
     Nf = 128
@@ -146,8 +162,8 @@ if __name__ == "__main__":
                           theta_step=step,
                           radius=r)
 
-    x_vec = np.array([0., -1., 0., 1.])
-    y_vec = np.array([1., 0., -1., 0.])
+    x_vec = np.array([-0.1, -0.5, 0.5, 0.1])
+    y_vec = np.array([0., 0., 0., 0.])
     z_vec = np.zeros(4)
 
     mics = np.array([x_vec,y_vec,z_vec]).T